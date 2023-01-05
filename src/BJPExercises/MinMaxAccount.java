4352a79eb46336c3a450b927e25f53c69af895f8
matui3 <jonformantes@gmail.com>
2023-01-05 11:47:39 -0800

    BJP Exercise 9.9 finished

--- src/BJPExercises/MinMaxAccount.java
+++ src/BJPExercises/MinMaxAccount.java
@@ -1,4 +1,46 @@
 package BJPExercises;
 
 public class MinMaxAccount extends BankingAccount {
+
+    private int min;
+    private int max;
+    public MinMaxAccount(Startup s) {
+        super(s);
+        min = s.startup_getBalance();
+        max = s.startup_getBalance();
+
+    }
+
+    private void min_max(int value) {
+        if (value > max) {
+            max = value;
+        }
+        if (value < min) {
+            min = value;
+        }
+    }
+
+    @Override
+    public void debit(Debit d) {
+        super.debit(d);
+        min_max(super.getBalance());
+    }
+
+    @Override
+    public void credit (Credit c) {
+        super.credit(c);
+        min_max(super.getBalance());
+    }
+
+    public int getMin() {
+
+        return min;
+    }
+
+    public int getMax() {
+
+
+        return max;
+    }
+
 }
---
+++ src/BJPExercises/main.java
@@ -0,0 +1,7 @@
+package BJPExercises;
+
+public class main {
+    public static void main(String[] args) {
+        BankingAccount one = new BankingAccount();
+    }
+}
